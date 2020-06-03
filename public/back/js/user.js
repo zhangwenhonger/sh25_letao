6d2724c10d8711a5b8e7eb0f2f62caf8babff820
zwh <1501490849@qq.com>
2020-06-03 18:29:25 +0800

    用户管理 启用禁用按钮完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -2,9 +2,10 @@
 $(function() {
     var currentPage=1;
     var pageSize=5;
+    var currentId;
+    var isDelete;
 //1.已进入页面发送ajax请求，获取用户列表数据，通过模板引擎渲染
     render();
-
     function render() {
         $.ajax({
             type: "get",
@@ -33,5 +34,39 @@ $(function() {
             }
         });
     }
+//2.点击禁用启用按钮，显示模态框，通过事件委托绑定事件
+    $('tbody').on("click",".btn",function(){
+        $('#userModal').modal("show");
+
+        //jquery提供了获取自定义属性的方法  获取当前id
+        currentId=$(this).parent().data("id");
+        // 如果是禁用按钮，说明需要将该用户设置成禁用状态，传0   判断给后台传0还是1
+        isDelete=$(this).hasClass("btn-danger")?0:1;
+
+    })
+//3.点击确认按钮，发送ajax请求，修改对应用户状态
+    $('#submitBtn').click(function(){
+        $.ajax({
+            type:"post",
+            url:"/user/updateUser",
+            data:{
+                id:currentId,
+                isDelete:isDelete
+            },
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    //关不模态框 重新渲染
+                    $("userModal").modal("hide");
+                    render();
+                }
+
+            }
+        })
+    })
+
+
+
 })
 
